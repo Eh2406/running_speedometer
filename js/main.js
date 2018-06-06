fff67bf356cec3636af5588844f4aaa907e6bee3
Finkelman <finkelman@semcog.org>
2018-06-05 23:35:16 -0400

    start button

--- js/main.js
+++ js/main.js
@@ -101,6 +101,8 @@ $(function () {
         })
     }
 
+    $("#start").click(reset)
+
     map = L.map('map');
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
